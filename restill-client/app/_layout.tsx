9b82981debbb64609f2b4420503391e16328f81e
Matthew Kitchens <mathiuskitchens@gmail.com>
2024-11-19 15:27:47 -0500

    removed comment

--- restill-client/app/_layout.tsx
+++ restill-client/app/_layout.tsx
@@ -38,8 +38,6 @@ export default function RootLayout() {
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
-          {/* <Stack.Screen name="(moodModals)/moodLogModal" options={{presentation: 'fullScreenModal', headerShown: false}} />
-          <Stack.Screen name="(moodModals)/thankfulModal"  /> */}
           <Stack.Screen name="(moodModals)" options={{ headerShown: false, presentation: 'fullScreenModal' }} />
         </Stack>
       </MoodProvider>
